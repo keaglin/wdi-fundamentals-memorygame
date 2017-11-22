571aa3ebf123caa3dc6295bcf1849e48dc11f34c
Kevon Eaglin <kceaglin@gmail.com>
2017-11-22 09:55:55 -0500

    added reset button

--- js/main.js
+++ js/main.js
@@ -16,35 +16,78 @@ var cardsInPlay = []
 var flipCounter = 0
 var cardIdArr = []
 
+// lifted from my cookie_clicker app (originally got the prototype code from 
+//internet and changed it around to suit my purpose)
+var hideButton = function () {
+  var el = document.getElementsByClassName('canHide')[0]
+  el.style.display = "none"
+}
+
+var showButton = function () {
+  var el = document.getElementsByClassName('canHide')[0]
+  el.style.display = "block"
+}
+
+var showHide = function () {
+  if (flipCounter) {
+    // debugger
+    showButton() 
+  } else {
+    hideButton()
+  }
+}
+
+//when I click reset, I want the score to go down to 0
+var resetButton = document.getElementsByClassName('reset')[0]
+resetButton.addEventListener('click', function() {
+//   debugger
+  resetBoard()
+  showHide()
+})
+
 var resetBoard = function() {
   for (var i = 0; i < cards.length; i++) {
     var resetCard = document.querySelectorAll('img')[i]
     resetCard.setAttribute('src', 'images/back.png')
   }
   cardsInPlay = []
+  cardIdArr = []
   flipCounter = 0
+}
+
+var clearAfterMatch = function() {
+  cardsInPlay = []
+  cardIdArr = []
 }
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!")
     alert('You found a match!')
+    // we want to clear cardsInPlay since you already matched 2 and we're gonna compare 2 more
+    clearAfterMatch()
   } else {
     console.log("Sorry, try again.")
     alert('Sorry, try again')
-    resetBoard()
+    // we actually want to just flip the card that doesn't match back not both so we don't want resetBoard, we want something like resetCard
+//     resetCard()
   }
   // how many cards have been turned over? if it's 4 or more, we want to resetBoard
   //how do we know a card has been turned over?
-  if (flipCounter >= 4) {
-    resetBoard()
-  }
+  // if (flipCounter > 0) {
+    // this is where we'd want to show a reset button instead of
+    // auto-resetting the board. could probably apply for every
+    // instance in which we reset the board
+    // it cannot bc when we click the button, we wanna then resetBoard
+    // showHide()
+  // }
 }
 
 var checkForDupe = function(cardId) {
   if (cardIdArr[0] === cardIdArr[1]) {
     alert('Please select 2 different cards. Try again!')
     cardsInPlay.pop()
+    cardIdArr.pop()
   }
 }
 
@@ -59,7 +102,8 @@ var flipCard = function() {
   // before counting, we wanna make sure the user hasn't clicked the same card twice
   checkForDupe(cardIdArr)
   ++flipCounter
-  console.log(flipCounter)
+  showHide()
+  // console.log(flipCounter)
   if (cardsInPlay.length > 1) {
     checkForMatch()
     cardsInPlay = []
@@ -77,6 +121,3 @@ var createBoard = function() {
 }
 
 createBoard()
-
-// flipCard(0)
-// flipCard(2)
